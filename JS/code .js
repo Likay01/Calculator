8b99b35a72ec84d68446a078b9c4f0b6fc00769f
Likay01 <mgoqilindokuhle2gmail.com>
2022-11-23 15:15:18 -0800

    Update

--- JS/code .js
+++ JS/code .js
@@ -1,9 +1,38 @@
 let output = document.getElementById("Display-bar");
+let allowComa = true;
 
 
 // appends button values to input field
 function display(anyVal) {
     output.value = output.value + anyVal;
+}
+
+function displayMin(anyVal) {
+    output.value = output.value + anyVal;
+    allowComa = true;
+    
+}
+function displayMult(anyVal) {
+    output.value = output.value + anyVal;
+    allowComa = true;
+    
+}
+function displayDiv(anyVal) {
+    output.value = output.value + anyVal;
+    allowComa = true;
+    
+}
+function displayPlus(anyVal) {
+    output.value = output.value + anyVal;
+    allowComa = true;
+    
+}
+
+function displayComa(anyVal) {
+    if (allowComa === true) {
+        output.value = output.value + anyVal;
+        allowComa = false;
+    }
 }
 
 
